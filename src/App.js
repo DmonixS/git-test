086a41f35b6a54d78eb3a1f210b292eb177f93cb
dmonix <b9ka.dima@yandex.ru>
2020-10-24 18:31:41 +0300

    first

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import Header from './components/Header';
+
+
+
+
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Header />
+    </div>
+  );
+}
+
+
+
+
+
+
+
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+    return (
+        <header className="header">
+            <div className="logo">
+                <img src="https://cdn.svgporn.com/logos/lookback.svg" />
+            </div>
+        </header>
+    )
+}
+
+export default Header;
